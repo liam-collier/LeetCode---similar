06dbda38c9f65d338e466136388279a0a731729d
liam-collier <liamcollier@gmx.de>
2023-11-07 23:47:43 +0000

    Changed IsUnique.c to make use of calloc

--- CtCI/IsUnique.c
+++ CtCI/IsUnique.c
@@ -8,29 +8,27 @@ Approach: Map each char to an array of booleans to check for duplicates in const
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define ARR_LEN 256                         //Set ARR_LEN to 256 for extended ASCII range.
+#define ARR_LEN 256                                     //Set ARR_LEN to 256 for extended ASCII range.
 
 bool isUnique(char*pStr) {
     int len = strlen(pStr);
-    if (len > ARR_LEN) {                    //Pigeon hole principle, more chars in str than possible unique chars.
+    if (len > ARR_LEN) {                                //Pigeon hole principle, more chars in str than possible unique chars.
         return false;
     }
-    bool Arr[ARR_LEN];                      //Initialise array of 256 false bools.
-    for (int i = 0; i < ARR_LEN; i++) {
-        Arr[i] = false;
-    }
-    for (int i = 0; i < len; i++) {         //Iterate over string.
-        int ch = *(pStr + i);               //Convert char to ASCII value.
-        if (Arr[ch]) {                      //Char already found, not unique string.
+    bool*pArr = (bool*)calloc(ARR_LEN, sizeof(bool));   //Initialise array of 256 false bools.
+    for (int i = 0; i < len; i++) {                     //Iterate over string.
+        int ch = *(pStr + i);                           //Convert char to ASCII value.
+        if (*(pArr + ch)) {                                  //Char already found, not unique string.
             return false;
         }
-        Arr[ch] = true;                     //Mark char as found in array.
+        *(pArr + ch) = true;                                 //Mark char as found in array.
     }
-    return true;                            //No duplicates found.
+    return true;                                        //No duplicates found.
 }
 
-int main(void) {                            //Run for testing.
+int main(void) {                                        //Run for testing.
     char lngstr[260];
     for (int i = 0; i < 260; i++) {
         lngstr[i] = 'a';
