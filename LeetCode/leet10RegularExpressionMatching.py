9367b3da1e0204c2659637d9469aec2431475217
liam-collier <liamcollier@gmx.de>
2023-11-26 00:00:32 +0000

    Added non functioning own solution to LeetCode 10

--- LeetCode/leet10RegularExpressionMatching.py
+++ LeetCode/leet10RegularExpressionMatching.py
@@ -1,6 +1,7 @@
 #This solution is heavily inspired by this video:
 #https://www.youtube.com/watch?v=HAA8mgxlov8&t=547s
 #All credits go to the creator "NeetCode"
+#My own (bad) version that passed 354/355 test cases is in the comments below
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
@@ -23,3 +24,87 @@ class Solution:
             return False
         
         return search(0, 0)
+    
+
+"""
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        sPointer = 0
+        pPointer = 0
+        pMemory = False
+        amount = 0
+        while sPointer < len(s):
+            if pPointer >= len(p):
+                if pMemory:
+                    pPointer = pMemory
+                    sPointer = sMemory
+                    sMemory += 1
+                    if p[pPointer] != ".":
+                        while sPointer < len(s) and s[sPointer] != p[pPointer]:
+                            sPointer += 1
+                    if sPointer == len(s):
+                        return False
+                else:
+                    return False
+            if pPointer != len(p) - 1 and p[pPointer + 1] == "*":
+                if p[pPointer] == ".":
+                    pPointer += 2
+                    while pPointer < len(p) - 1 and p[pPointer + 1] == "*":
+                        pPointer += 2
+                    if pPointer == len(p):
+                        return True
+                    sMemory = sPointer + 1
+                    if  p[pPointer] != ".":
+                        while sPointer < len(s) and s[sPointer] != p[pPointer]:
+                            sPointer += 1
+                    if sPointer == len(s):
+                        return False
+                    pMemory = pPointer
+                    continue
+                prev_s = sPointer
+                while sPointer < len(s) and s[sPointer] == p[pPointer]:
+                    sPointer += 1
+                amount = sPointer - prev_s
+                pPointer += 2
+                cont = True
+                while pPointer < len(p) and amount and cont:
+                    if p[pPointer] != "." and pPointer != len(p) - 1 and p[pPointer + 1] == "*" and (sPointer == len(s) or s[sPointer] != p[pPointer]):
+                        pPointer += 2
+                    elif p[pPointer] == s[sPointer - 1]:
+                        amount -= 1
+                        pPointer += 1
+                    else:
+                        cont = False
+                continue
+            if p[pPointer] == ".":
+                pPointer += 1
+                sPointer += 1
+                continue
+            if p[pPointer] != s[sPointer]:
+                if pMemory:
+                    pPointer = pMemory
+                    sPointer = sMemory
+                    sMemory += 1
+                    if p[pPointer] != ".":
+                        while sPointer < len(s) and s[sPointer] != p[pPointer]:
+                            sPointer += 1
+                    if sPointer == len(s):
+                        return False
+                else:
+                    return False
+            pPointer += 1
+            sPointer += 1
+        if pPointer == len(p):
+            return True
+        while pPointer < len(p):
+            print("Status: ", pPointer, p[pPointer])
+            if pPointer != len(p) - 1 and p[pPointer + 1] == "*":
+                pPointer += 2
+            elif p[pPointer] == "." and amount:
+                amount -= 1
+                pPointer += 1
+            else:
+                return False
+        print("Reached end", pPointer, len(p))
+        return pPointer == len(p)
+"""
